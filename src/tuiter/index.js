48c4f3ade12e53bdb5c355cbaa37c88e558f3916
Bino <you@example.com>
2023-02-20 13:18:14 -0500

    3.5 done

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,20 @@
 import Nav from "../nav";
+import NavigationSidebar
+    from "./navigation-sidebar";
+import WhoToFollowListItem
+    from "./who-to-follow-list/";
+import PostSummaryList from "./post-summary-list";
+
 function Tuiter() {
     return (
         <div>
             <Nav/>
+            <NavigationSidebar active="home"/>
+            <WhoToFollowListItem/>
+            <PostSummaryList/>
             <h1>Tuiter</h1>
         </div>
     )
 }
+
 export default Tuiter
