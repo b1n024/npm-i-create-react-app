1f0cbd611098071d3d1f54bbb7d70631a362b38c
Bino <you@example.com>
2023-03-13 13:47:30 -0400

    labs done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
+import Assignment7 from "./labs/a7";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 
@@ -15,6 +16,8 @@ function App() {
                            element={<HelloWorld/>}/>
                     <Route path="/tuiter/*"
                            element={<Tuiter/>}/>
+                    <Route path="/a7"
+                            element={<Assignment7/>}/>
 
                 </Routes>
             </div>
