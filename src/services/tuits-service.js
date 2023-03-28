7f4ed3e4a034e543490dd9dec78591d259f499ec
Bino <you@example.com>
2023-03-28 11:35:04 -0400

    change to environment var

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const TUITS_API = 'https://tuiter-node-server-app-u1yb.onrender.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
+
 export const updateTuit = async (tuit) => {
     const response = await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
