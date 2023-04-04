40bdcf2a981268afd40b1ca0ea6e80c010725682
Bino <you@example.com>
2023-04-03 20:14:41 -0400

    hard coded api url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = 'https://tuiter-node-server-app-a8-mdyl.onrender.com';
 const TUITS_API = `${API_BASE}/tuits`;
 console.log(TUITS_API)
 
